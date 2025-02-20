a3ea4ffad51bc398460e7fe5047e78d697f62721
Erik Pedersen <erik.pedersen138@gmail.com>
2025-02-21 09:41:32 +1100

    added POST request to file-server.py

--- file-server.py
+++ file-server.py
@@ -15,32 +15,37 @@ with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
                 break
             try:
                 method, file, *_ = data.split(b" ")
+                file = file.strip(b"\n")
                 print(f"DEBUG: method = {method}, file = {file}")
             except ValueError:
-                conn.sendall(b"Usage: <METHOD> <FILENAME>")
+                conn.sendall(b"Usage: <GET/POST> <FILENAME>")
                 continue
 
             if method == b"GET":
                 try:
-                    f = open(file)
-                    send = f.read().encode(encoding="utf-8")
+                    f = open(file, "r")
+                    send = f.read().encode()
                     conn.send(file + b": \n")
                     conn.sendall(send)
+                    f.close()
                 except FileNotFoundError:
                     print(f"File {f} not found.")
 
             elif method == b"POST":
-                try:
-                    f = open(file, "w")
-                    conn.sendall(b"Send data to transmit\n")
-                    while True:
-                        postData = conn.recv(1024)
-                        if not postData:
-                            f.close()
-                            break
-                        f.write(postData)
-                except:
-                    conn.sendall(b"Error receiving data")
+                #try:
+                f = open(file, "w")
+                conn.sendall(b"Send data to transmit\n")
+                while True:
+                    postData = conn.recv(1024)
+                    if postData == b"EOF" or postData == b"EOF\n":
+                        conn.sendall(b"Data received, closing file.\n")
+                        f.close()
+                        break
+                    elif not postData:
+                        conn.sendall(b"Failed to receive file")
+                        f.close()
+                        break
+                    f.write(postData.decode())
             else:
                 send = b"Invalid method.\n"
                 conn.sendall(send)
